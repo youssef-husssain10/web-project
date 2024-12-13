86805488dc60448171a784a0ec448528fb60a2bb
youssef-husssain10 <ys2201282@tkh.edu.eg>
2024-12-13 22:21:40 +0200

    customers

--- server.js
+++ server.js
@@ -34,3 +34,89 @@ const verifyToken = (req, res, next) => {
         next();
     })
 }
+// 1. POST /customers - Register a new customer
+server.post(`/customers`, (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+            return res.status(500).send('error hashing password')
+        }
+        db.run(`INSERT INTO CUSTOMER (NAME,EMAIL,PASSWORD,ISADMIN) VALUES (?,?,?,?)`, [name, email, hashedPassword, 0], (err) => {
+            if (err) {
+
+                return res.status(401).send(err)
+            }
+            else
+                return res.status(200).send(`registration successfull`)
+        })
+    })
+})
+
+// 2. POST /login - Authenticate a customer (check email/password)
+server.post('/login', (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    db.get('SELECT * FROM CUSTOMER WHERE EMAIL = ?', [email], (err, row) => {
+        if (err || !row) {
+            return res.status(500).send('Error finding user')
+        }
+
+        bcrypt.compare(password, row.PASSWORD, (err, isMatch) => {
+            if (err) {
+                return res.status(500).send('Error comparing password.')
+            }
+            if (!isMatch) {
+                return res.status(401).send('Invalid credentials')
+            }
+            else { }
+            let userID = row.ID
+            let isADMIN = row.ISADMIN
+            const token = generateToken(userID, isADMIN);
+
+            res.cookie('authToken', token, {
+                httpOnly: true,
+                sameSite: 'none',
+                secure: true,
+                expiresIn: '1h'
+
+            })
+            return res.status(200).send({ id: userID, admin: isADMIN });
+        })
+    })
+})
+
+//#########################################################################################################
+
+// 3. GET /customers- Get all customers
+server.get('/customers', verifyToken, (req, res) => {
+    const ISADMIN = req.userDetails.isAdmin;
+    if (ISADMIN !== 1) {
+        return res.status(403).send('Forbidden');
+    }
+    db.get('SELECT * FROM CUSTOMER ', (err, row) => {
+        if (err) {
+            console.log(err)
+            return res.status(404).send(err);
+        }
+        res.status(200).send(row)
+    })
+})
+
+// 4. DELETE /customers/:id - Delete a customer
+server.delete('/customers/:id', verifyToken, (req, res) => {
+    const customer = `DELETE FROM CUSTOMER WHERE ID = ${req.params.id}`
+    const ISADMIN = req.userDetails.isAdmin;
+    // Ensure that only the customer or an admin can delete the data
+    if (ISADMIN !== 1) {
+        return res.status(403).send('Forbidden');
+    }
+    db.run(customer, (err) => {
+        if (err) {
+            return res.status(500).send('Error deleting customer')
+        }
+        res.status(200).send('Customer deleted successfully')
+    })
+})
